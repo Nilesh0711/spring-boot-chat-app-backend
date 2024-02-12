61f4cfd1052bb80e1442edf4f79234b45126fd8a
Nilesh Gupta <nilgupta2002@gmail.com>
2024-02-12 23:11:50 +0530

    feat: isTyping socket

--- src/main/java/com/nilesh/whatsappclone/controller/RealTimeChat.java
+++ src/main/java/com/nilesh/whatsappclone/controller/RealTimeChat.java
@@ -6,8 +6,17 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nilesh.whatsappclone.model.Chat;
 import com.nilesh.whatsappclone.model.Message;
+import com.nilesh.whatsappclone.model.User;
 
+import io.jsonwebtoken.io.IOException;
+
+import java.util.Map;
 import java.util.logging.Logger;
 
 @RestController
@@ -28,5 +37,30 @@ public class RealTimeChat {
         simpMessagingTemplate.convertAndSend(destination, message);
         logger.info("Received message: " + message.getContent());
         return message;
+    }
+
+    @MessageMapping("/message/typing")
+    @SendTo("/group/typing/public")
+    public User userTypingMessage(@Payload String payload)
+            throws InterruptedException, JsonMappingException, JsonProcessingException {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> payloadMap = objectMapper.readValue(payload, new TypeReference<Map<String, Object>>() {
+            });
+
+            User user = objectMapper.convertValue(payloadMap.get("user"), User.class);
+            Chat chat = objectMapper.convertValue(payloadMap.get("chat"), Chat.class);
+
+            String destination = "/group/typing/" + chat.getId().toString();
+            simpMessagingTemplate.convertAndSend(destination, user);
+
+            logger.info("Received user Typing: " + user.getEmail());
+            logger.info("Received user Typing: " + chat.getId());
+
+            return user;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
